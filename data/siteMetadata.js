eb4447cb8b058e0b90f002fb9d4477992c292a3e
gptkong <gptkong>
2025-05-30 11:35:49 +0800

    Update tag data counts and change site language and locale to Simplified Chinese

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   author: 'GPTKong',
   headerTitle: 'Access Blog',
   description: '还能做点什么呢',
-  language: 'en-us',
+  language: 'zh-Hans-CN',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
@@ -22,7 +22,7 @@ const siteMetadata = {
   instagram: 'https://www.instagram.com',
   medium: 'https://medium.com',
   bluesky: 'https://bsky.app/',
-  locale: 'en-US',
+  locale: 'zh-Hans-CN',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
