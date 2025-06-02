549de9b11e4f41725343841fb51e5ea1084e7ce0
gptkong <gpt@kong.vision>
2025-06-02 19:17:24 +0800

    Update site metadata with new URL and repository, and remove Twitter discussion link from PostLayout for a cleaner interface.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: '还能做点什么呢',
   language: 'zh-Hans-CN',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://oaico.de',
+  siteRepo: 'https://github.com/gptkong/next-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
